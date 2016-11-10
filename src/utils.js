40a7225bc8b41ea6bfdf2ae0fff87de37cce2d4d
Raistlin916 <raistlin916@gmail.com>
2016-11-10 15:55:53 +0800

    improve

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,31 @@
-export default {
-  debounce: (() => {
-    let tid = null;
-    return (fn, delay) => {
-      if (tid) {
-        return
-      }
-      tid = setTimeout(() => {
-        tid = null
-      }, delay)
-      fn()
-    };
-  })(),
-  getRandomInt: (min, max) =>
+import Vector from 'victor'
+
+const debounce = (() => {
+  let tid = null;
+  return (fn, delay) => {
+    if (tid) {
+      return
+    }
+    tid = setTimeout(() => {
+      tid = null
+    }, delay)
+    fn()
+  };
+})();
+
+const getRandomInt = (min, max) =>
     Math.floor(Math.random() * (max - min + 1)) + min
+
+const truncate = (origin, max) => {
+  const clone = origin.clone()
+  if (origin.length() <= max) {
+    return clone
+  }
+  return clone.norm().multiply({x: max, y: max})
+}
+
+export {
+  debounce,
+  getRandomInt,
+  truncate
 }
